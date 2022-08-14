a99bbafc20f31229f0df7d575d7b1df1a92c1576
RINZIN DORJI <rinzin.dorji@selise.ch>
2022-08-14 14:12:13 +0600

    feat: inherite from actioncontroller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class GraphqlController < ApplicationController
+class GraphqlController < ActionController::API
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
@@ -11,8 +11,7 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user
     }
     result = BlogSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
