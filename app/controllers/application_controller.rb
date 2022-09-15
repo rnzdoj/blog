f314fa60a465b478b8e4717791861b813ecb845c
RINZIN DORJI <rinzin.dorji@selise.ch>
2022-09-15 17:28:38 +0600

    error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
   respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.is_a?(Hash) then super
+    elsif resource.errors.present? then render json: error_message, status: :unprocessable_entity
+    else
+      render json: user_params(resource)
+    end
+  end
+
+  def error_message
+    { errors: resource.errors.full_messages }
+  end
 end
