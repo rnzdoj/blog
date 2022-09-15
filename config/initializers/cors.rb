50031942c924effd1aba03b352b7220cedc730c4
RINZIN DORJI <rinzin.dorji@selise.ch>
2022-09-15 17:01:34 +0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000', 'http://127.0.0.1:4000/'
+    origins '*'
     resource '*',
              headers: :any,
-             credentials: true,
              methods: %i[get post patch put]
   end
 end
